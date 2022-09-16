6df36435c88199b435fa0ddf5d0356d142fb174f
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-16 23:09:27 +0300

    adding yellow and red toses to divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
+const boxes = document.querySelectorAll(".box")
+const box1 = document.querySelector("#mak")
+let playerRed ={}
+let playerYellow ={}
+let currentPlayer = playerYellow
 
+for(let i=0;i<boxes.length;i++){
+    boxes[i].addEventListener("click",()=>{
+        if(boxes[i].children.length==1){
+            return
+        }else{
+            let img = document.createElement('img');
+            if(currentPlayer==playerYellow){
+                img.src = "assets/yellow.png";
+                img.classList.add("images")
+                boxes[i].appendChild(img);
+                currentPlayer=playerRed 
+            }else{
+                img.src = "assets/red.png";
+                img.classList.add("images")
+                boxes[i].appendChild(img);
+                currentPlayer=playerYellow
+            }
+        }
+      
+    })
+}
